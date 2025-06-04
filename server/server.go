98eaa69366da5d90fad67d0eae3eab661f523e79
alexVinchent <alexvinchent312.talenta@gmail.com>
2025-06-04 20:59:08 +0700

    fix: update comment untuk server

--- server/server.go
+++ server/server.go
@@ -11,7 +11,7 @@ func main() {
 	// membuat koneksi untuk mendengar protocol tcp alamat 9090
 	listenConection, err := net.Listen("tcp", ":9090")
 
-	// check jka
+	// check jka error membuat connection akan keluar pesan
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to listen!")
 		os.Exit(1)
@@ -19,8 +19,10 @@ func main() {
 		fmt.Println("Listening...")
 	}
 
+	// menerima koneksi client
 	conn, err := listenConection.Accept()
 
+	// jika tidak bisa menerima maka error
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to accept connection!")
 		os.Exit(1)
@@ -28,9 +30,11 @@ func main() {
 		fmt.Println("New connection accepted!")
 	}
 
+	// menerima pesan dari client
 	reader := bufio.NewReader(conn)
 
 	fmt.Println("Waiting for message...")
+	// check jika berhasil membaca maka
 	message, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to read message!")
@@ -39,6 +43,7 @@ func main() {
 		fmt.Println("The message has been received!")
 	}
 
+	// kirim pesan ke client
 	fmt.Fprintf(conn, "Echo: %s", message)
 	fmt.Println("The message has been echoed back!")
 }

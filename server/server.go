b609250e6b10d1f1e8c4f9081d4bf90725111cb4
alexVinchent <alexvinchent312.talenta@gmail.com>
2025-06-05 10:31:04 +0700

    feat: make server accept multiple client with identity

--- server/server.go
+++ server/server.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+// sturcture identity user
+type User struct {
+	name    string
+	koneksi net.Conn
+}
+
+// untuk simpan user yang login (Resource: need mutex if multiple user want to change or append)
+var listUser = []User{}
 
 func main() {
 	// membuat koneksi untuk mendengar protocol tcp alamat 9090
@@ -19,31 +29,62 @@ func main() {
 		fmt.Println("Listening...")
 	}
 
-	// menerima koneksi client
-	conn, err := listenConection.Accept()
+	for {
+		// menerima koneksi client
+		conm, err := listenConection.Accept()
 
-	// jika tidak bisa menerima maka error
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to accept connection!")
-		os.Exit(1)
-	} else {
-		fmt.Println("New connection accepted!")
+		// jika tidak bisa menerima maka error
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to accept connection!")
+			os.Exit(1)
+		} else {
+			fmt.Println("New connection accepted!")
+		}
+
+		go communicationWithServer(conm)
 	}
+
+}
+
+func communicationWithServer(koneksiUser net.Conn) {
 
 	// menerima pesan dari client
-	reader := bufio.NewReader(conn)
+	reader := bufio.NewReader(koneksiUser)
 
-	fmt.Println("Waiting for message...")
+	fmt.Println("Waiting for user insert name")
 	// check jika berhasil membaca maka
-	message, err := reader.ReadString('\n')
+	name, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read message!")
-		os.Exit(1)
-	} else {
-		fmt.Println("The message has been received!")
+		fmt.Fprintf(os.Stderr, "Failed to read name: %v\n", err)
+		return
 	}
 
-	// kirim pesan ke client
-	fmt.Fprintf(conn, "Echo: %s", message)
-	fmt.Println("The message has been echoed back!")
+	name = strings.TrimSpace(name)
+	fmt.Printf("User '%s' has joined.\n", name)
+
+	// Register user
+	user := User{
+		name:    name,
+		koneksi: koneksiUser,
+	}
+
+	listUser = append(listUser, user)
+
+	// Inform user
+	fmt.Fprintf(koneksiUser, "Welcome, %s! Type a message:\n", name)
+
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("User '%s' disconnected.\n", name)
+			return
+		}
+
+		message = strings.TrimSpace(message)
+		fmt.Printf("[%s] %s\n", user.name, message)
+
+		// Echo back to the client
+		fmt.Fprintf(koneksiUser, "Echo [%s]: %s\n", user.name, message)
+	}
+
 }

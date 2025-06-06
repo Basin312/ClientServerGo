76db80c54a6ce56892ad56f7fd9d0e3280a4c0bb
SamuelSurjoSaputro <6182201007@student.unpar.ac.id>
2025-06-06 13:32:40 +0700

    Fitur utama

--- client/client.go
+++ client/client.go
@@ -5,8 +5,19 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
-
+func listenMessages(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Disconnected from server.")
+			os.Exit(0)
+		}
+		fmt.Print(msg)
+	}
+}
 func main() {
 	// bikin koneksi type network dan alamat port
 	conn, err := net.Dial("tcp", ":9090")
@@ -18,31 +29,24 @@ func main() {
 		fmt.Println("Connected to server!")
 	}
 
-    // untuk menerima pesan dari server
-	connReader := bufio.NewReader(conn)
-    // input reader untuk keyboard
-	localReader := bufio.NewReader(os.Stdin)
-    
-	fmt.Print("Type your message> ")
-	
-    // membaca localreader sampai user klik enter
-    message, err := localReader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot read the message!")
-	} else {
-		fmt.Println("The message has been read!")
-	}
+	go listenMessages(conn)
 
-	conn.Write([]byte(message)) // fmt.Fprint(conn, message)
-	fmt.Println("The message has been sent!")
-	fmt.Println("Waiting for reply...")
-	echo, err := connReader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read the echo!")
-		os.Exit(1)
-	} else {
-		fmt.Println("The echo has been received!")
-	}
+	//input username dari client
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	conn.Write([]byte(name + "\n"))
 
-	fmt.Println(echo)
+
+    for{
+		fmt.Print("You> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+		if text == "exit" {
+			break
+		}
+		conn.Write([]byte(text + "\n"))
+
+	}
 }
--- server/server.go
+++ server/server.go
@@ -5,8 +5,18 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
+	"strings"
 )
-
+type Client struct {
+	conn net.Conn
+	name string
+}
+var (
+	clients   = make(map[net.Conn]Client) // koneksi aktif
+	broadcast = make(chan string)         // channel untuk siaran pesan
+	lock      sync.Mutex                  // untuk sinkronisasi akses map
+)
 func main() {
 	// membuat koneksi untuk mendengar protocol tcp alamat 9090
 	listenConection, err := net.Listen("tcp", ":9090")
@@ -18,32 +28,64 @@ func main() {
 	} else {
 		fmt.Println("Listening...")
 	}
-
+	go broadcaster()
 	// menerima koneksi client
-	conn, err := listenConection.Accept()
-
-	// jika tidak bisa menerima maka error
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to accept connection!")
-		os.Exit(1)
-	} else {
-		fmt.Println("New connection accepted!")
+	for{
+		conn, err := listenConection.Accept()
+	
+		// jika tidak bisa menerima maka error
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to accept connection!")
+			os.Exit(1)
+		} else {
+			fmt.Println("New connection accepted!")
+		}
+		go handleClient(conn)
 	}
 
-	// menerima pesan dari client
+}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
 	reader := bufio.NewReader(conn)
 
-	fmt.Println("Waiting for message...")
-	// check jika berhasil membaca maka
-	message, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read message!")
-		os.Exit(1)
-	} else {
-		fmt.Println("The message has been received!")
-	}
+	conn.Write([]byte("Enter your name: "))
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
 
-	// kirim pesan ke client
-	fmt.Fprintf(conn, "Echo: %s", message)
-	fmt.Println("The message has been echoed back!")
+	lock.Lock()
+	clients[conn] = Client{conn: conn, name: name}
+	lock.Unlock()
+
+	broadcast <- fmt.Sprintf(">> %s has joined the chat\n", name)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		message = strings.TrimSpace(message)
+		if message != "" {
+			broadcast <- fmt.Sprintf("[%s]: %s\n", name, message)
+		}
+	}
+	lock.Lock()
+	delete(clients, conn)
+	lock.Unlock()
+	broadcast <- fmt.Sprintf(">> %s has left the chat\n", name)
+}
+func broadcaster() {
+	for {
+		msg := <-broadcast
+		lock.Lock()
+		for conn := range clients {
+			go func(c net.Conn) {
+				_, err := c.Write([]byte(msg))
+				if err != nil {
+					c.Close()
+					delete(clients, c)
+				}
+			}(conn)
+		}
+		lock.Unlock()
+	}
 }
